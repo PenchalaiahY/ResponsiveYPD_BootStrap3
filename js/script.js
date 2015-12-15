99a236ec9962d45dfb1f55c309ba08804a836cb0
PenchalaiahY <yekula.penchalaiah@wipro.com>
2015-12-15 17:23:22 +0530

    Responsiveness implemented. Pending for code rew.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
-/* script to set the height of left hero element */
-$('#left_hero').css('height', $('#right_hero').height()+'px');
-$('#contentWrapper').css('margin-top',$('#hero').height()/4+'px');
-
+/* script to set the Margin of left hero element */
+$('#contentWrapper').css('margin-top',$('#hero').height()/6+'px');
+$('#contentWrapper').css('margin-left',$('#left_hero').width()/20+'px');
 $(window).on('resize', function(event){
-    $('#left_hero').css('height', $('#right_hero').height()+'px');
-    $('#contentWrapper').css('margin-top',$('#hero').height()/4+'px');
+    $('#contentWrapper').css('margin-top',$('#hero').height()/6+'px');
+    $('#contentWrapper').css('margin-left',$('#left_hero').width()/20+'px');
+});
+
+/* Modal selector */
+$('.trow').click(function(event){
+  $(this).css('background-color','#F0F0F0');
 });
