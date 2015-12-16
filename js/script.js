0d0bd893037af7061caba18c71a15471ccb66673
PenchalaiahY <yekula.penchalaiah@wipro.com>
2015-12-16 17:30:43 +0530

    Updated code and folder structure

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-/* script to set the Margin of left hero element */
-$('#contentWrapper').css('margin-top',$('#hero').height()/6+'px');
-$('#contentWrapper').css('margin-left',$('#left_hero').width()/20+'px');
-$(window).on('resize', function(event){
-    $('#contentWrapper').css('margin-top',$('#hero').height()/6+'px');
-    $('#contentWrapper').css('margin-left',$('#left_hero').width()/20+'px');
-});
-
 /* Modal selector */
-$('.trow').click(function(event){
-  $(this).css('background-color','#F0F0F0');
+$(function(){
+  $('.trow').click(function(event){
+    $(this).css('background-color','#F0F0F0');
+  });
+
+  $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="popover"]').popover(); 
+  $("#purchase").click(function(e){
+    $("#successAlert").slideDown();
+  })
 });
